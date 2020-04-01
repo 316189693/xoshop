6cd26e8120010bffa7736831cfdc9253b192f84b
will zheng <will.zheng@unisco.com>
2020-04-01 14:19:31 +0800

    login

--- src/main/java/com/xo/shop/Demo.java
+++ src/main/java/com/xo/shop/Demo.java
@@ -5,6 +5,8 @@ import com.xo.shop.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,16 +23,23 @@ import javax.activation.MimeType;
 @Api(value="dashboard")
 @Controller
 public class Demo {
+    Logger logger = LoggerFactory.getLogger(Demo.class);
  @GetMapping("/dashboard")
 
     public String test(){
      return "test01";
  }
 
-    @GetMapping(value = "/dashboard01", produces = "application/json")
+    @GetMapping(value = "/dashboard01")
     @ApiOperation(value = "desc of test01", notes = "")
     @ResponseBody
     public User test01(@ApiParam(value = "desc of param" , required=true ) @RequestParam String name){
+        logger.trace("trace level");
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
+
         User user = new User();
         user.setEmail("123@123.com");
         user.setFirst_name("dddd");
